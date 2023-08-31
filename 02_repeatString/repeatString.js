d58a622eb5bc2efe19295e0ba4afe5342ce49989
Aiden <aidenpham@ucsb.edu>
2023-08-31 16:35:23 -0700

    Finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let count = 0;
+    let s = "";
+    while (count < num){
+        s += string;
+    }
+    return s;
 };
 
 // Do not edit below this line
