b823a9146bce673e62bef4408ef966b41ebf8ff2
Aiden <aidenpham@ucsb.edu>
2023-08-31 17:00:18 -0700

    Finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (const a of args){
+        let index = array.indexOf(a);
+        if (index > -1){
+            array.splice(index, 1);
+        }
+        continue;
+    }
+    return array;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
