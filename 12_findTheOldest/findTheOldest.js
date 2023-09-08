5e6c05fe70af32f5c9ade04eef37bd55ee28eb92
Aiden <aidenpham@ucsb.edu>
2023-09-08 02:31:25 -0700

    Finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        return oldestAge < currAge ? current : oldest;
+    });
 };
+
+function getAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
