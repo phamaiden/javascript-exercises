b6582be6cbdb5d16e1895e4ec24a3fd1f0e1ccf8
Aiden <aidenpham@ucsb.edu>
2023-09-08 01:51:02 -0700

    Finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+    if (count < 0) return "OOPS";
+    if (count === 0) return 0;
 
+    let firstPrev = 1;
+    let secPrev = 0;
+
+    for (let i = 2; i <= count; i++){
+        let sum = firstPrev + secPrev;
+        secPrev = firstPrev;
+        firstPrev = sum;
+    }
+    return firstPrev;
 };
 
 // Do not edit below this line
